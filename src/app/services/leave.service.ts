592bf8c2da8a53d36bceb9cdaaae5130b2b3d68f
Kashish <kashishgupta221003@gmail.com>
2026-05-14 09:59:50 +0530

    Initial commit

---
+++ src/app/services/leave.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LeaveService {
+  // private apiUrl = 'https://leave-management-backend-427919814652.asia-south1.run.app';
+  // test
+  // test 2
+  private apiUrl = 'http://127.0.0.1:8000';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  // });
+private getHeaders(): HttpHeaders {
+  const token = this.authService.getToken();
+
+  return new HttpHeaders({
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json'
+  });
+}
+
+  submitLeave(leaveData: any): Observable<any> {
+    console.log('Submitting leave with data:', leaveData);
+    return this.http.post(`${this.apiUrl}/leaves/submit`, leaveData, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  getMyLeaves(): Observable<any> {
+    console.log('Fetching my leaves...');
+    return this.http.get(`${this.apiUrl}/leaves/my-leaves`, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  getAllLeaves(): Observable<any> {
+    // console.log('Fetching all leaves...');
+    // return this.http.get(`${this.apiUrl}/leaves/all-leaves`, {
+    //   headers: this.getHeaders(),
+    // });
+     const token = this.authService.getToken();
+
+    const headers = new HttpHeaders({
+    Authorization: `Bearer ${token}`
+  });
+
+  console.log("Fetching all leaves...");
+
+  return this.http.get(`${this.apiUrl}/leaves/all-leaves`, { headers });
+  }
+
+  updateLeaveStatus(leaveId: string, status: string): Observable<any> {
+    console.log(`Updating leave ${leaveId} status to ${status}`);
+    return this.http.put(`${this.apiUrl}/leaves/${leaveId}/status?status=${status}`, {}, {
+      headers: this.getHeaders(),
+    });
+  }
+  
+  updateLeave(leaveId: string, leaveData: any): Observable<any> {
+  return this.http.put(
+    `${this.apiUrl}/leaves/${leaveId}`,
+    leaveData,
+    { headers: this.getHeaders() }
+  );
+  }
+  deleteLeave(leaveId: string): Observable<any> {
+    console.log(`Deleting leave ${leaveId}`);
+    return this.http.delete(`${this.apiUrl}/leaves/${leaveId}`, {
+      headers: this.getHeaders(),
+    });
+  }
+
+  getPendingLeaves(): Observable<any> {
+    console.log('Fetching pending leaves...');
+    return this.http.get(`${this.apiUrl}/leaves/pending`, {
+      headers: this.getHeaders(),
+    });
+  }
+}
